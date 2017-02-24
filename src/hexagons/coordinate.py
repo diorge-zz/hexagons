a9e9be19bb7e706b934318fbb41a8116a2c0cd5a
diorge <diorge.bs@gmail.com>
2017-02-24 14:55:22 -0300

    Checks for valid cube coordinates

--- src/hexagons/coordinate.py
+++ src/hexagons/coordinate.py
@@ -11,6 +11,8 @@ class Cube:
     """
 
     def __init__(self, x, y, z):
+        if x + y + z != 0:
+            raise ValueError('Cube does not slice the x+y+z=0 plane')
         self._x = x
         self._y = y
         self._z = z
--- src/hexagons/test/test_coordinates.py
+++ src/hexagons/test/test_coordinates.py
@@ -4,13 +4,14 @@ Test module for coordinate methods and conversions
 
 
 import hexagons.coordinate as coord
+import pytest
 
 
 def test_cube_getters():
-    c = coord.Cube(1, 0, -3)
+    c = coord.Cube(1, 0, -1)
     assert c.x == 1
     assert c.y == 0
-    assert c.z == -3
+    assert c.z == -1
 
 
 def test_cube_to_axis():
@@ -21,3 +22,9 @@ def test_cube_to_axis():
 def test_axis_to_cube():
     assert coord.Axial(1, -3).to_cube() == coord.Cube(1, 2, -3)
     assert coord.Axial(-2, 5).to_cube() == coord.Cube(-2, -3, 5)
+
+
+def test_invalid_cube():
+    """ Cubes are invalid if they don't slice the x+y+z=0 plane """
+    with pytest.raises(ValueError):
+        c = coord.Cube(1, 2, 3)
